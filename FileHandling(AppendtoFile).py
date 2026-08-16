c9666fb5448f242f4c317dae89ddc0320e01355a
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-16 22:19:47 +0500

    added

---
+++ FileHandling(AppendtoFile).py
@@ -0,0 +1,17 @@
+# Ask for filename
+a = input("Enter a fileName: ")
+
+# Ask for text to add
+b = input("Enter a text to add: ")
+
+# Open file in append mode
+file = open(a, "a")
+
+# Write text to file
+file.write(b + "\n")  # \n adds a new line
+
+# Close file
+file.close()
+
+# Success message
+print("Text added successfully!")
