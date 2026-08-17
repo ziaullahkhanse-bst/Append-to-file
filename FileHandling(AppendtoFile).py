bf126311f30e4a102fdbd379d7b8e9819bfdb7ca
Ziaullah Khan <ziaullahkhan.se@gmail.com>
2026-08-17 21:24:56 +0500

    Update FileHandling(AppendtoFile).py

--- FileHandling(AppendtoFile).py
+++ FileHandling(AppendtoFile).py
@@ -2,10 +2,8 @@ a = input("Enter a fileName: ")
 
 b = input("Enter a text to add: ")
 
-# Open file in append mode
 file = open(a, "a")
 
-# Write text to file
 file.write(b + "\n")  # \n adds a new line
 
 # Close file
